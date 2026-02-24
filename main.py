74f84f8728c1018b00b6506f65b789f5cdcabac5
Gregório Kessler Steinke <42988007+gregoriok@users.noreply.github.com>
2026-02-24 17:34:48 -0300

    Ajuste inicial

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/hello")
 def hello():
     return {"message": "hello world"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app,host="localhost", port=8000)
